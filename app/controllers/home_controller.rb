4ef53b08134d4be2ab171d53cdeeb5a78d601395
Tom Tuddenham <ferrisoxide@gmail.com>
2024-02-17 19:56:35 +1030

    rough ui code, using tailwind examples

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,15 @@
 # frozen_string_literal: true
 
-# Public: Home controller
+# Home controller presents the landing page for the application (when no user is signed in),
+# and the dashboard (when a user is signed in).
 class HomeController < ApplicationController
   skip_before_action :authenticate_user!, raise: false
 
-  def index; end
+  def index = render(template)
+
+  private
+
+  def template
+    user_signed_in? ? 'dashboard' : 'landing_page'
+  end
 end
